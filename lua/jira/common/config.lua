b4bae705e401c7437a97f7233afcb24c14bd9434
letieu <letieu8@gmail.com>
2025-12-31 16:53:13 +0700

    chore: add next sprint query to default config

--- lua/jira/common/config.lua
+++ lua/jira/common/config.lua
@@ -27,6 +27,8 @@ M.defaults = {
   },
   projects = {},
   queries = {
+    ["Next sprint"] = "project = '%s' AND sprint in futureSprints() ORDER BY Rank ASC",
+    ["Backlog"] = "project = '%s' AND (issuetype IN standardIssueTypes() OR issuetype = Sub-task) AND (sprint IS EMPTY OR sprint NOT IN openSprints()) AND statusCategory != Done ORDER BY Rank ASC",
     ["My Tasks"] = "assignee = currentUser() AND statusCategory != Done ORDER BY updated DESC",
   },
 }
